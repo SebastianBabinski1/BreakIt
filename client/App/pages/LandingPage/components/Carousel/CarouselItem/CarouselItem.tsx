dc9d5d0042e15e82981c34dae760c86ea5ffd85c
Elfant <alexs98@wp.pl>
2022-06-27 22:26:25 +0200

    fix: reversed conditional change

--- client/App/pages/LandingPage/components/Carousel/CarouselItem/CarouselItem.tsx
+++ client/App/pages/LandingPage/components/Carousel/CarouselItem/CarouselItem.tsx
@@ -14,7 +14,7 @@ const CarouselItem = ({ slide, stopSlide, startSlide, text }: Props) => (
   <div className={styles.carouselItem} onMouseOver={stopSlide} onMouseOut={startSlide}>
     <div className={styles.carouselContentWrapper}>
       <img className={classNames(styles.carouselImage, { [styles.withoutText]: text == '' })} src={slide} />
-      {text !== 'x' && (
+      {text !== '' && (
         <>
           <p className={styles.carouselText}>{text}</p>
           <img className={styles.arrow} src={arrowRight} />
